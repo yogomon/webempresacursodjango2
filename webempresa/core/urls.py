03ef02cb7fbee9eaf80ffc4c666082c76483114c
yogomon <yogomon@gmail.com>
2020-05-01 20:39:52 +0200

    añadido proyecto

---
+++ webempresa/core/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+        # Paths del core
+    path('', views.home, name="home"),
+    path('about', views.about, name="about"),
+    path('store', views.store, name="store"),
+    path('contact', views.contact, name="contact"),
+    path('sample', views.sample, name="sample"),
+]
